43d25f9ff8a0edfe879cf7536438eff88993710c
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2025-03-26 11:32:15 -0600

    se agrego el display

--- Test20250326.AppWebMVC/Models/Warehouse.cs
+++ Test20250326.AppWebMVC/Models/Warehouse.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Test20250326.AppWebMVC.Models;
 
 public partial class Warehouse
 {
     public int Id { get; set; }
-
+    [Display(Name = "Nombre")]
     public string WarehouseName { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
